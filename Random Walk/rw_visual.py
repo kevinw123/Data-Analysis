cecd02bb15d0315df08e5ecd9f9eeb7cdbf9a316
Kevin Wong <kevinwong_895@hotmail.com>
2016-07-03 00:24:34 -0700

    Adding size of plotting window

--- Random Walk/rw_visual.py
+++ Random Walk/rw_visual.py
@@ -4,15 +4,21 @@ from random_walk import RandomWalk
 # Keep making new walks as long as program is active
 while True:
 	# Make a random walk & plot points
-	rw = RandomWalk()
+	rw = RandomWalk(50000)
 	rw.fill_walk()
 
+	# set size of plotting window
+	plt.figure(dpi=128,figsize=(10,6))
 	point_numbers = list(range(rw.num_points))
-	plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
+	plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
 
 	# Emphasize first and last points
 	plt.scatter(0, 0, c='green', edgecolors='none', s=100)
 	plt.scatter(rw.x_values[-1],rw.y_values[-1],c='red',edgecolors='none',s=100)
+
+	# Remove axes
+	plt.axes().get_xaxis().set_visible(False)
+	plt.axes().get_yaxis().set_visible(False)
 	plt.show()
 
 	keep_running = input("Make another walk? (y/n): ")
