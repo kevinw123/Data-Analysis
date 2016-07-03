330714d991fc549d618831471f69edf5baa0713d
Kevin Wong <kevinwong_895@hotmail.com>
2016-07-03 14:36:43 -0700

    Adding die_visual image and changed file for two die

--- Dice/die_visual.py
+++ Dice/die_visual.py
@@ -1,19 +1,29 @@
 from die import Die
+import pygal
 
-#Create 6 sided die
-die = Die()
+#Create two 6 sided die
+die_1 = Die()
+die_2 = Die()
 
 # Make some rolls and store results in a list
 results = []
 for roll_num in range(1000):
-	result = die.roll()
+	result = die_1.roll() + die_2.roll()
 	results.append(result)
 
 # Analyze results
 frequencies = []
-for value in range(1, die.num_sides +1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result +1):
 	frequency = results.count(value)
 	frequencies.append(frequency)
 
+# Visual the results
+hist = pygal.Bar()
+hist.title = "Results of rolling two 6-sided Die 1000 times"
+hist.x_labels = ['2','3', '4', '5','6', '7','8', '9', '10','11', '12']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
 
-print(frequencies)
+hist.add('D6 + D6', frequencies)
+hist.render_to_file('die_visual.svg')
