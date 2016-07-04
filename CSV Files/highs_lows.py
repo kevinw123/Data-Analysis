b564cadb958e0e4557114b4ca31de1b69315ff15
Kevin Wong <kevinwong_895@hotmail.com>
2016-07-03 22:42:15 -0700

    Plotting data

--- CSV Files/highs_lows.py
+++ CSV Files/highs_lows.py
@@ -1,5 +1,5 @@
 import csv
-
+from matplotlib import pyplot as plt
 # Get high temperatures from file
 filename = 'sitka_weather_07-2014.csv'
 with open(filename) as f:
@@ -10,3 +10,15 @@ with open(filename) as f:
         high = int(row[1])
         highs.append(row[1])
     print(highs)
+
+# Plot data
+fig = plt.figure(dpi=128, figsize=(10,6))
+plt.plot(highs, c='red')
+
+#Format 
+plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.xlabel('',fontsize=16)
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major',labelsize=16)
+
+plt.show()
