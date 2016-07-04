ccb520780787c09bc493f339f2c4325a389c0c10
Kevin Wong <kevinwong_895@hotmail.com>
2016-07-03 22:49:57 -0700

    Adding a second plot with date time

--- CSV Files/highs_lows.py
+++ CSV Files/highs_lows.py
@@ -1,23 +1,31 @@
 import csv
 from matplotlib import pyplot as plt
-# Get high temperatures from file
-filename = 'sitka_weather_07-2014.csv'
+from datetime import datetime
+
+# Get dates, high and low temperatures from file
+filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    highs = []
+    dates, highs, lows = [], [], []
     for row in reader:
+    	current_date = datetime.strptime(row[0], "%Y-%m-%d")
+    	dates.append(current_date)
         high = int(row[1])
         highs.append(row[1])
+        low = int(row[3])
+        lows.append(low)
     print(highs)
 
 # Plot data
 fig = plt.figure(dpi=128, figsize=(10,6))
-plt.plot(highs, c='red')
+plt.plot(dates, highs, c='red')
+plt.plot(dates, lows, c='blue')
 
 #Format 
-plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.title("Daily high and low temperatures - 2014", fontsize=24)
 plt.xlabel('',fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major',labelsize=16)
 
