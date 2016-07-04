2d124e19beb5a65225e241c9d42a98970b6754b6
Kevin Wong <kevinwong_895@hotmail.com>
2016-07-03 23:09:04 -0700

    Filling between plot

--- CSV Files/highs_lows.py
+++ CSV Files/highs_lows.py
@@ -12,16 +12,15 @@ with open(filename) as f:
     	current_date = datetime.strptime(row[0], "%Y-%m-%d")
     	dates.append(current_date)
         high = int(row[1])
-        highs.append(row[1])
+        highs.append(high)
         low = int(row[3])
         lows.append(low)
-    print(highs)
 
 # Plot data
 fig = plt.figure(dpi=128, figsize=(10,6))
-plt.plot(dates, highs, c='red')
-plt.plot(dates, lows, c='blue')
-
+plt.plot(dates, highs, c='red',alpha=0.5)
+plt.plot(dates, lows, c='blue',alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue',alpha=0.1)
 #Format 
 plt.title("Daily high and low temperatures - 2014", fontsize=24)
 plt.xlabel('',fontsize=16)
